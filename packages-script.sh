410434aa1da17fb4a533fcc12974417f56f17cb1
Konstantin <98838016+Qantrex@users.noreply.github.com>
2023-12-14 12:43:35 +0000

    added packages and cleaned up code

--- packages-script.sh
+++ packages-script.sh
@@ -1,27 +1,33 @@
 #!/bin/bash
 
+font_array=("otf-font-awesome" "ttf-jetbrains-mono-nerd" "ttf-jetbrains-mono" "otf-font-awesome-4" "ttf-droid" "ttf-fantasque-sans-mono" "adobe-source-code-pro-fonts" "noto-fonts-emoji")
+base_pkgs=("base-devel" "wget" "sudo" "git" "zip" "unzip")
+baseApps_pkgs=("hyprland" "kitty" "jq" "mako" "waybar-hyprland" "swww" "swaylock-effects" "wofi" "thunar" "pavucontrol" "brightnessctl" "sddm-git" "sddm-sugar-candy-git")
+software_pkgs=("nodejs" "npm" "audacious" "btop" "neofetch" "firefox" "tor" "jetbrains-toolbox" "nvim" "code" "thunderbird" "gimp" "libreoffice-fresh" "geogebra" "kcalc" "flatpak" "gparted" "xreader" "firewalld" "keepassxc" "vlc" "obsidian" "onedrive" "discord" "steam")
+
+# $EUID is the variable that hold the "effective user ID."
+# The EUID for root is 0
 # Check if running as root. If root, script will exit
 if [[ $EUID -eq 0 ]]; then
     echo "[ERROR] This script should not be executed as root! Exiting......."
     exit 1
 fi
 
-# Set some colors for output messages
+# "tput setaf x" sets color to x
+# "tput sgr0" reset color to standard
+# Basic output messages
 OK="$(tput setaf 2)[OK]$(tput sgr0)"
 ERROR="$(tput setaf 1)[ERROR]$(tput sgr0)"
 NOTE="$(tput setaf 3)[NOTE]$(tput sgr0)"
 WARN="$(tput setaf 166)[WARN]$(tput sgr0)"
 CAT="$(tput setaf 6)[ACTION]$(tput sgr0)"
 
+# Redirects all output to the log files
 LOG_FILE="install.log"
-
-# Redirect all output and errors to the log file
 exec > >(tee -i "$LOG_FILE") 2>&1
 
 # Exit on any error
 set -e
-
-software_array=("audacious" "firefox" "tor" "jetbrains-toolbox" "nvim" "code" "thunderbird" "gimp" "libreoffice-fresh" "geogebra" "kcalc" "flatpak" "gparted" "xreader" "firewalld" "keepassxc" "vlc" "obsidian" "onedrive" "discord" "steam")
 
 install_package() {
     if yay -Q "$1" &>> /dev/null; then
@@ -40,11 +46,16 @@ install_package() {
     fi
 }
 
+perform_system_update() {
+    printf "\n%s - Performing a full system update to avoid issues.... \n" "${NOTE}"
+    yay
+}
+
 cd ~/
 clear
 
 # Check for AUR helper and install yay if not found
-ISAUR=$(command -v yay || command -v paru)
+ISAUR=$(command -v yay)
 if [ -n "$ISAUR" ]; then
     printf "\n%s - AUR helper was located, moving on.\n" "${OK}"
 else 
@@ -56,14 +67,21 @@ else
     cd ..
 fi
 
-printf "\n%s - Performing a full system update to avoid issues.... \n" "${NOTE}"
-yay
+perform_system_update
 
-for element in "${software_array[@]}"; do
-    install_package "$element"
+# INSTALL PACKAGES
+
+for pkgs in "${base_pkgs[@]}"; do
+    install_package "$pkgs"
 done
 
-font_array=("otf-font-awesome" "ttf-jetbrains-mono-nerd" "ttf-jetbrains-mono" "otf-font-awesome-4" "ttf-droid" "ttf-fantasque-sans-mono" "adobe-source-code-pro-fonts" "noto-fonts-emoji")
+for pkgs in "${baseApps_pkgs[@]}"; do
+    install_package "$pkgs"
+done
+
+for pkgs in "${software_pkgs[@]}"; do
+    install_package "$pkgs"
+done
 
 for font in "${font_array[@]}"; do
     install_package "$font"
